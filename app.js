aeefe8c4fdfff55ed615dcf42c321196822c3dfd
unknown <erdematik8@gmail.com>
2022-12-21 17:00:28 +0300

    added geo coord to UTM

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-
+var utmObj = require('utm-latlng');
+const { multiply,add } = require('mathjs')
+var utm=new utmObj(); //Default Ellipsoid is 'WGS 84'
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
+
+const res = utm.convertLatLngToUtm( 41.016718, 28.985154, 100);
+console.log(res);
+
+
+Math.radians = function(degrees) {
+	return degrees * Math.PI / 180;
+}
+
+const rotPar = [[1, Math.radians(0.4738/3600), Math.radians(0.0003/3600)], 
+                [Math.radians(-0.4738/3600), 1, Math.radians(0.0183/3600)],
+                [Math.radians(-0.0003/3600), Math.radians(-0.0183/3600), 1]]
+
+const wgs84coord = [3869416.913, 2830423.682, 4192997.6984]
+
+const scaleWgs84 = -1.0347/1000000
+
+const offsetXYZt = [84.003, 102.315, 129.879]
+
+const rotatedCoord = multiply(rotPar,wgs84coord);
+
+const scaledCoord = wgs84coord.map(el=>el*scaleWgs84)
+
+const sum1= add(rotatedCoord, scaledCoord, offsetXYZt)
+
+console.log(sum1);
+
+
 var url = '../helloWorldLoc.pdf';
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
@@ -14,8 +44,10 @@ const PAGE_SCALE = 1.5;
 const SVG_NS = "http://www.w3.org/2000/svg";
 
 const pageforward = function(){
-  
+
 }
+
+pageforward()
 
 function buildSVG(viewport, textContent) {
     // Building SVG with size of the viewport (for simplicity)
