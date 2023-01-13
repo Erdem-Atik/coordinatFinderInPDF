3d251f9c85b998db9faf26def572dda6415dea0c
unknown <erdematik8@gmail.com>
2023-01-14 01:53:51 +0300

    integrated dec conv. to app

---
+++ src/app.js
@@ -0,0 +1,88 @@
+import {parseLatLong} from './latLongToDec.js';
+
+
+const url = new URL('../helloWorldLoc.pdf', import.meta.url);
+
+const grid = document.querySelector(".grid");
+
+let img = document.createElement('iframe');
+img.src = new URL('../helloWorldLoc.pdf', import.meta.url);
+document.body.appendChild(img);
+
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+const pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+const PAGE_NUMBER = 1; // it should not be hard coded!
+const PAGE_SCALE = 1.5;
+const SVG_NS = "http://www.w3.org/2000/svg";
+
+const pageforward = function(){
+
+}
+
+pageforward()
+
+function buildSVG(viewport, textContent) {
+    // Building SVG with size of the viewport (for simplicity)
+    const svg = document.createElementNS(SVG_NS, "svg:svg");
+    svg.setAttribute("width", viewport.width + "px");
+    svg.setAttribute("height", viewport.height + "px");
+    // items are transformed to have 1px font size
+    svg.setAttribute("font-size", 1);
+  
+    // processing all items
+    textContent.items.forEach(function (textItem) {
+      // we have to take in account viewport transform, which includes scale,
+      // rotation and Y-axis flip, and not forgetting to flip text.
+      const tx = pdfjsLib.Util.transform(
+        pdfjsLib.Util.transform(viewport.transform, textItem.transform),
+        [1, 0, 0, -1, 0, 0]
+      );
+      const style = textContent.styles[textItem.fontName];
+      // adding text element
+      const text = document.createElementNS(SVG_NS, "svg:text");
+      text.setAttribute("transform", "matrix(" + tx.join(" ") + ")");
+      text.setAttribute("font-family", style.fontFamily);
+      text.textContent = textItem.str;
+      svg.append(text);
+    });
+    return svg;
+  }
+
+  async function pageLoaded() {
+    // Loading document and page text content
+    const loadingTask = pdfjsLib.getDocument(url);
+    const pdfDocument = await loadingTask.promise;
+    const page = await pdfDocument.getPage(PAGE_NUMBER);
+    const viewport = page.getViewport({ scale: PAGE_SCALE });
+    const textContent = await page.getTextContent();
+
+    textContent.items.forEach(el=>{
+
+      if(el.str.includes('°') )
+      {console.log(el.str);}
+      });
+
+    const geoInPDF = textContent.items.filter(el=>{
+      return el.str.includes('°')
+    }) 
+
+    // building SVG and adding that to the DOM
+    const svg = buildSVG(viewport, textContent);
+    document.getElementById("pageContainer").append(svg);
+    // Release page resources.
+    page.cleanup();
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    if (typeof pdfjsLib === "undefined") {
+      // eslint-disable-next-line no-alert
+      alert("Please build the pdfjs-dist library using\n  `gulp dist-install`");
+      return;
+    }
+    pageLoaded();
+  });  
---
+++ src/latLongToDec.js
@@ -0,0 +1,25 @@
+
+export function parseLatLong(input) {
+
+    if(!( input.toUpperCase() != input.toLowerCase())) {   // if geodirection abbr. isn't exist, it should be already decimal notation or not valid format
+        return `${input}:the coordinate already seems as decimal or not valid format`
+    }
+    const parts = input.split(/[°'"]+/).join(' ').split(/[^\w\S]+/); // thanks to Shannon Antonio Black(StackoverFlow) for regEx patterns 
+    const replacedComa = parts.map(el=>el.replace(',','.')) // in case the given coord. numbers include comma as decimal separator
+    const geoLetters = replacedComa.filter(el=> !(+el))
+    geoLetters.filter(el=>{return el !== ''} ); // if the array contains empty string, it will remove. Above regex pattern sometimes make side effect
+    const coordNumber = replacedComa.filter(n=>(+n)).map(nr=>+nr)
+    const latNumber = coordNumber.slice(0,(coordNumber.length/2))
+    const longNumber = coordNumber.slice((coordNumber.length/2))
+    const reducer = function(acc,coord,curInd){               
+       return (acc + (coord/Math.pow( 60, curInd++ )))
+    }
+    let latDec = latNumber.reduce(reducer)
+    let longDec = longNumber.reduce(reducer)
+    
+    if((geoLetters[0].toUpperCase()==='S')||(geoLetters[0].toUpperCase()==='G')) latDec = -latDec // G is abr. of güney(it means South in Turkish)
+    if((geoLetters[1].toUpperCase()==='W')||(geoLetters[1].toUpperCase()==='B')) longDec= -longDec // B is abr. of batı (it means west in Turkish)
+    
+    
+    return [latDec,longDec]
+    }
