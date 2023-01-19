9a76a26de98f8978e9fd77c0ccc31ab5d422d46e
unknown <erdematik8@gmail.com>
2023-01-19 23:55:08 +0300

    added git ignore

--- src/app.js
+++ src/app.js
@@ -1,84 +1,46 @@
 import {parseLatLong} from './latLongToDec.js';
-
+const pdfjsLib = require('pdfjs-dist');
 
 const input = document.getElementById('pdf-file')
 const uploadButton = document.getElementById('upload-button')
 
-const url = new URL('../helloWorldLoc.pdf', import.meta.url);
+uploadButton.addEventListener('click', function() {
+  const file = input.files[0]
+  if (!file) {
+      alert('Please select a file!')
+      return
+  }
 
-const grid = document.querySelector(".grid");
-
-let img = document.createElement('iframe');
-//img.src = new URL('../helloWorldLoc.pdf', import.meta.url);
-//document.body.appendChild(img);
-
-// Loaded via <script> tag, create shortcut to access PDF.js exports.
-const pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-// The workerSrc property shall be specified.
-pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-
-const PAGE_NUMBER = 1; // it should not be hard coded!
-const PAGE_SCALE = 1.5;
-const SVG_NS = "http://www.w3.org/2000/svg";
+  const reader = new FileReader()
+  reader.onload = function() {
+      const pdfData = new Uint8Array(reader.result)
+      console.log(pdfData);
+  }
+  reader.readAsArrayBuffer(file)
+  console.log( reader.readAsArrayBuffer(file));
+})
 
 
-function buildSVG(viewport, textContent) {
-    // Building SVG with size of the viewport (for simplicity)
-    const svg = document.createElementNS(SVG_NS, "svg:svg");
-    svg.setAttribute("width", viewport.width + "px");
-    svg.setAttribute("height", viewport.height + "px");
-    // items are transformed to have 1px font size
-    svg.setAttribute("font-size", 1);
-  
-    // processing all items
-    textContent.items.forEach(function (textItem) {
-      // we have to take in account viewport transform, which includes scale,
-      // rotation and Y-axis flip, and not forgetting to flip text.
-      const tx = pdfjsLib.Util.transform(
-        pdfjsLib.Util.transform(viewport.transform, textItem.transform),
-        [1, 0, 0, -1, 0, 0]
-      );
-      const style = textContent.styles[textItem.fontName];
-      // adding text element
-      const text = document.createElementNS(SVG_NS, "svg:text");
-      text.setAttribute("transform", "matrix(" + tx.join(" ") + ")");
-      text.setAttribute("font-family", style.fontFamily);
-      text.textContent = textItem.str;
-      svg.append(text);
+
+function parsePDF(pdfData) {
+    pdfjsLib.getDocument(pdfData).promise.then(function(pdf) {
+        pdf.getPage(1).then(function(page) {
+            var scale = 1.5;
+            var viewport = page.getViewport({scale: scale});
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+              canvasContext: context,
+              viewport: viewport
+            };
+            page.render(renderContext);
+        });
     });
-    return svg;
-  }
+}
 
-  async function pageLoaded() {
-    // Loading document and page text content
-    const loadingTask = pdfjsLib.getDocument(url);
-    const pdfDocument = await loadingTask.promise;
-    const page = await pdfDocument.getPage(PAGE_NUMBER);
-    const viewport = page.getViewport({ scale: PAGE_SCALE });
-    const textContent = await page.getTextContent();
-
-
-    let geoCoordInPDF = [];
-    textContent.items.forEach(el=>{
-      if(el.str.includes('°')&&el.str.includes(`'`)){
-        geoCoordInPDF.push(el.str)
-      }
-    })
-    console.log(parseLatLong(geoCoordInPDF.join(' ')));
-
-    // building SVG and adding that to the DOM
-    const svg = buildSVG(viewport, textContent);
-    document.getElementById("pageContainer").append(svg);
-    // Release page resources.
-    page.cleanup();
-  }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    if (typeof pdfjsLib === "undefined") {
-      // eslint-disable-next-line no-alert
-      alert("Please build the pdfjs-dist library using\n  `gulp dist-install`");
-      return;
-    }
-    pageLoaded();
-  });  
